4ae47051c12df9898f6e31eb111f380ed8e480c9
Filipe Eduardo Regis <47003717+regisrc@users.noreply.github.com>
2022-02-04 14:05:49 -0300

    Mudança na url

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://10.1.1.20/api",
+  baseURL: "https://app.thematchcontrol.com.br:4443/api",
   headers: {
     'Access-Control-Allow-Origin': '*'
   }
